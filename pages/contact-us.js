454aa4087e54332510f5ab56657760c9c4333304
Stephen <Stephenbech19@gmail.com>
2022-09-02 02:44:45 +0100

    finish

---
+++ pages/contact-us.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Head from 'next/head';
+import Link from 'next/link'
+function ContactUs() {
+  return (
+    <div>
+          <Head>
+               <title>Contact Us Page</title>
+          </Head>
+
+          Yoooooooo This is stephensons page
+          <Link href= '/' >HOME PAGE</Link>
+    </div>
+    
+  )
+}
+
+export default ContactUs;
